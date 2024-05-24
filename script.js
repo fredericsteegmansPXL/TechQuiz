5d5285d20e5a0e02240857c72ff9d5607c73c81e
fredericsteegmansPXL <frederic.steegmans@student.pxl.be>
2024-05-25 00:37:35 +0200

    imageConatiner toegevoegd en juist laten inladen

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const quizData = [
     {
-        question: "What is the capital of France?",
-        options: ["Paris", "Rome", "Berlin", "Madrid"],
-        answer: "Paris"
+        question: "Wat is de term voor het proces waarbij computers zelfstandig leren en verbeteren zonder expliciete programmering?",
+        imagePath: "assets/vis5.png", // Path to the image for the first question
+        options: ["Zelflerend programmeren", "Kunstmatige intelligentie", "Machine learning", "Augmented reality"],
+        answer: "Machine learning"
     },
     {
         question: "What is 2 + 2?",
+        imagePath: "math.jpg", // Path to the image for the second question
         options: ["3", "4", "5", "6"],
         answer: "4"
     },
     {
         question: "Which planet is known as the Red Planet?",
+        imagePath: "mars.jpg", // Path to the image for the third question
         options: ["Jupiter", "Mars", "Saturn", "Neptune"],
         answer: "Mars"
     }
@@ -21,6 +24,8 @@ const optionsElement = document.getElementById("options");
 const submitButton = document.getElementById("submit");
 const restartButton = document.getElementById("restart");
 const resultElement = document.getElementById("result");
+const imageContainer = document.getElementById("image-container");
+
 
 let currentQuestion = 0;
 let score = 0;
@@ -29,11 +34,14 @@ let userAnswer = null;
 loadQuiz();
 
 function loadQuiz() {
+
     // Remove inline display styles
     questionElement.style.removeProperty('display');
     optionsElement.style.removeProperty('display');
+    imageContainer.style.removeProperty('display');
 
     const currentQuizData = quizData[currentQuestion];
+    imageContainer.querySelector("img").src = currentQuizData.imagePath;
     questionElement.innerText = currentQuizData.question;
     optionsElement.innerHTML = "";
     currentQuizData.options.forEach(option => {
@@ -75,10 +83,13 @@ function showResult() {
 
     questionElement.style.display = "none";
     optionsElement.style.display = "none";
+    imageContainer.style.display = "none"; // Hide image container
+
+
     quizData.forEach((quizItem, index) => {
         const question = document.createElement("p");
         question.textContent = `${index + 1}. ${quizItem.question}`;
-        const answer = document.createElement("p");
+        const answer = document.createElement("h4");
         answer.textContent = `${quizItem.answer}`;
         resultElement.appendChild(question);
         resultElement.appendChild(answer);
